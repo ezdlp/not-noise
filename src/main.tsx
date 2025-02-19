860e7a579b9a38acdded3bb0599070f840f1c004
ezdlp <ezequieldelaparra@gmail.com>
2025-02-19 15:28:01 +0100

    Vercel Analytics exclude links

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
+import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
+import App from './App.tsx';
+import './index.css';
+import { useLocation, BrowserRouter as Router } from 'react-router-dom';
 
-import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Analytics } from '@vercel/analytics/react'
-import App from './App.tsx'
-import './index.css'
+const Root = () => {
+  const location = useLocation();
+  const shouldTrack = !location.pathname.includes('/link/');
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <SpeedInsights />
-    <Analytics />
-  </>
+  return (
+    <>
+      <App />
+      <SpeedInsights />
+      {shouldTrack && <Analytics />}
+    </>
+  );
+};
+
+createRoot(document.getElementById('root')!).render(
+  <Router>
+    <Root />
+  </Router>
 );
-
