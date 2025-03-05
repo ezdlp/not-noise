33638460d5698df825804c658a2acbe9c0e1ea6a
ezdlp <ezequieldelaparra@gmail.com>
2025-03-05 19:35:45 +0100

    Fixed analytics

---
+++ supabase/migrations/20240325000000_add_utility_functions.sql
@@ -0,0 +1,10 @@
+-- Add utility functions needed by other migrations
+
+-- Function to update the updated_at column automatically
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql; 
