887ec90e63eb8a3c242f39912555971388086ecd
ezdlp <ezequieldelaparra@gmail.com>
2025-04-10 15:54:14 -0300

    Test supa deploy

--- supabase/functions/verify-payment-session/index.ts
+++ supabase/functions/verify-payment-session/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.208.0/http/server.ts";
 import Stripe from 'https://esm.sh/stripe@15.7.0'; // Updated to match version in create-checkout-session
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
 
